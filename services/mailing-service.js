14008fbd830d94eecf35166c6533f0776c9c5097
Sathira <sathiraumesh@gmail.com>
2018-09-03 11:08:23 +0530

    added the mailing service functionality

---
+++ services/mailing-service.js
@@ -0,0 +1,25 @@
+const nodeMailer=require("nodemailer");
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport("smtp",{
+  service: 'gmail',
+  auth: {
+    user: '',
+    pass: ''
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'lrmamweerasinghe@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
